5551173b2d463717e3b0e4286802f7910c5a1223
Joseph-Irving <joseph.irving500@gmail.com>
2016-01-05 16:24:23 +0000

    BC #comment fixed java for centos

--- Puppet/Modules/java/init.pp
+++ Puppet/Modules/java/init.pp
@@ -1,12 +1,20 @@
 class java {
+ 
+  case $operatingsystem {
+    'Ubuntu': { 
+       package {'default-jdk':
+        ensure => present,
+        before => Package['default-jre'],
+       }
 
-  package {'default-jdk':
-    ensure => present,
-	before => Package['default-jre']
-  }
-
-  package {'default-jre':
-    ensure => present,
-  }
-
-}  
+       package {'default-jre':
+         ensure => present,
+       }
+    }
+    'CentOS': {
+     package {'java-1.7.0-openjdk':
+       ensure => present,
+     }
+    }
+  }  
+}
