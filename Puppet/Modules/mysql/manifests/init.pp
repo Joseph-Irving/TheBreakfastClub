c773803b8e7fff711137af348e2f2c2ce52506c3
unknown <jforbes@PC-CZC4164ZNH.netbuilder.private>
2016-01-06 09:27:19 +0000

    #comment updated

--- Puppet/Modules/mysql/manifests/init.pp
+++ Puppet/Modules/mysql/manifests/init.pp
@@ -9,28 +9,28 @@ class mysql{
 	logoutput => true,
   }
   
-   exec { 'downloadwget'
+   exec { 'downloadwget':
     user    => root,
     cwd     => '/usr/local',
     command => 'sudo yum -y install wget',
 	notify  =>Exec['downloadmysql'],
   }
   
-    exec { 'downloadmysql'
+    exec { 'downloadmysql':
     user    => root,
     cwd     => '/usr/local',
     command => 'wget http://repo.mysql.com/mysql57-community-release-el7-7.noarch.rpm',
 	notify  =>Exec['runInstaller'],
   }
   
-  exec { 'runInstaller'
+  exec { 'runInstaller':
     user    => root,
     cwd     => '/usr/local',
     command => 'sudo rpm -ivh mysql57-community-release-el7-7.noarch.rpm',
 	notify  =>Exec['installmysql'],
   }
   
-  exec { 'installmysql'
+  exec { 'installmysql':
     user    => root,
     cwd     => '/usr/local',
     command => 'sudo yum install mysql-server || sudo /sbin/service mysqld start',
@@ -38,28 +38,28 @@ class mysql{
 	notify  =>Exec['installmariadb'],
   }
   
-  exec { 'installmariadb'
+  exec { 'installmariadb':
     user    => root,
     cwd     => '/usr/local',
     command => 'sudo yum install mariadb-server mariadb',
     notify  =>Exec['openTCPport'],
   }
   
-  exec { 'openTCPport'
+  exec { 'openTCPport':
     user    => root,
     cwd     => '/usr/local',
     command => 'iptables -A INPUT -i eth0 -p tcp -m tcp --dport 3306 -j ACCEPT',
     notify  =>Exec['mysqldstart'],
   }
   
-  exec { 'mysqldstart'
+  exec { 'mysqldstart':
     user    => root,
     cwd     => '/usr/local',
     command => 'sudo /sbin/service mysqld start',
     notify  =>Exec['startmariadb'],
   }
   
-  exec { 'startmariadb'
+  exec { 'startmariadb':
     user    => root,
     cwd     => '/usr/local',
     command => 'sudo systemctl start mariadb.service',
