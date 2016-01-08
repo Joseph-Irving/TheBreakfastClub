3345fa3ab2671f93b8df4e66c68f21b86d87673d
Joseph-Irving <joseph.irving500@gmail.com>
2016-01-08 16:25:40 +0000

    BC-13 #comment mongodb module

---
+++ Puppet/Modules/mongodb/manifests/init.pp
@@ -0,0 +1,38 @@
+class mongodb {
+   
+   Exec {
+    path => [
+	  '/usr/local/bin',
+	  '/opt/local/bin',
+	  '/usr/bin',
+	  '/bin',
+	  '/sbin'],
+	logoutput => true,
+	timeout => 0,
+  }
+  
+  file {'/etc/yum.repos.d/mongodb.repo':
+    ensure => present,
+	source => 'puppet:///modules/mongodb/mongodb.repo',
+	owner  => root,
+  }
+  
+  exec {'install_mongodb':
+    command => 'yum install -y mongodb-org',
+	require => File['/etc/yum.repos.d/mongodb.repo'],
+	user    => root,
+	before  => Exec['restart_mongod']
+  }
+  
+  exec {'restart_mongod':
+    command => '/etc/init.d/mongod restart'
+	user    => root,
+	before  => Exec['auto_mongod']
+  }
+  
+  exec {'auto_mongod':
+    command => 'chkconfig mongod on',
+	user    => root,
+  }
+
+}
