789b2ed692f9044412fc9285caf7534fd3e77c85
Joseph-Irving <joseph.irving500@gmail.com>
2016-01-14 12:00:15 +0000

    BC-12 #comment graylog stuff

--- Puppet/Modules/graylog/manifests/install.pp
+++ Puppet/Modules/graylog/manifests/install.pp
@@ -1,5 +1,16 @@
 class graylog::install {
   
+  Exec {
+    path => [
+	  '/usr/local/bin',
+	  '/opt/local/bin',
+	  '/usr/bin',
+	  '/bin',
+	  '/sbin'],
+	logoutput => true,
+	timeout => 0,
+  }
+  
   exec {'graylog_rpm':
     command => 'rpm -Uvh https://packages.graylog2.org/repo/packages/graylog-1.3-repository-el7_latest.rpm',
 	user    => root,
@@ -9,31 +20,43 @@ class graylog::install {
   exec {'install_graylog':
     command => 'yum install graylog-server graylog-web',
 	user    => root,
-	before  => Exec['start_grayserver'],
+	before  => Exec['set_secretgray'],
+  }
+  
+   file {'/etc/graylog/web/graylog-web-interface.conf':
+    ensure => present,
+	source => 'puppet:///modules/graylog/graylog-web-interface.conf',
+	owner  => root,
+  }
+  
+   exec {'set_secretgray':
+    command => "sed -i '11s/.*/password_secret = gfjftgdfsddhjfghcdgfrxzsdxszghfyudxstrh/' /etc/graylog/server/server.conf",
+	user    => root,
+	before  => Exec['setshapass_gray'],
+  }
+  
+  exec {'setshapass_gray':
+    command => "sed -i '22s/.*/root_password_sha2 = d003852b8e43f74fa8ee45ed72a2de32534ebac9478dced04e17f4b79442c487/' /etc/graylog/server/server.conf",
+	user    => root,
+	before  => Exec['start_grayserver']
   }
 	
   exec {'start_grayserver':
     command => 'systemctl start graylog-server',
 	user    => root,
-	before  => Exec['start_greyweb']
+	before  => Exec['start_greyweb'],
+	require => File['/etc/graylog/web/graylog-web-interface.conf'],
   }
 	
   exec {'start_greyweb':
     command => 'systemctl start graylog-web',
     user    => root,
-    before  => 
-  }
-
-  exec {'set_secretgray':
-    command => "sed -i '11s/.*/password_secret = gfjftgdfsddhjfghcdgfrxzsdxszghfyudxstrh/' /etc/graylog/server/server.conf",
-	user    => root,
-  }
-	
-  exec {'setshapass_gray':
-    command => "sed -i '22s/.*/root_password_sha2 = d003852b8e43f74fa8ee45ed72a2de32534ebac9478dced04e17f4b79442c487/' /etc/graylog/server/server.conf",
-	user    => root,
+    before  => Exec['run_greyweb'],
   }
   
-  file {'/etc/graylog/web/graylog-web-interface.conf':
-
+  exec {'run_greyweb':
+    command => './graylog-web-interface',
+    cwd     => '/usr/share/graylog-web/bin/',
+	user    => root,
+  }
 }
